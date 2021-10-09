bbc5085f7e0c59ed7c074a2b4632384a80343209
Anas <amanabbasi3660@gmail.com>
2021-10-09 23:21:06 +0530

    create game board

--- Minesweeper Game/app.js
+++ Minesweeper Game/app.js
@@ -19,8 +19,68 @@ document.addEventListener("DOMContentLoaded", () => {
       square.classList.add(shuffleArray[i]);
       grid.appendChild(square);
       squares.push(square);
+
+      //   Normal Click
+      square.addEventListener("click", (e) => {
+        click(square);
+      });
+    }
+
+    // add numbers
+    for (let i = 0; i < squares.length; i++) {
+      let total = 0;
+      const isLeftEdge = i % width === 0;
+      const isRightEdge = i % width === width - 1;
+
+      if (squares[i].classList.contains("valid")) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb"))
+          total++;
+        if (
+          i > 9 &&
+          !isRightEdge &&
+          squares[i + 1 - width].classList.contains("bomb")
+        )
+          total++;
+        if (i > 10 && squares[i - width].classList.contains("bomb")) total++;
+        if (
+          i > 11 &&
+          !isLeftEdge &&
+          squares[i - 1 - width].classList.contains("bomb")
+        )
+          total++;
+        if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb"))
+          total++;
+        if (
+          i < 90 &&
+          !isLeftEdge &&
+          squares[i - 1 + width].classList.contains("bomb")
+        )
+          total++;
+        if (
+          i < 88 &&
+          !isRightEdge &&
+          squares[i + 1 + width].classList.contains("bomb")
+        )
+          total++;
+        if (i < 89 && squares[i + width].classList.contains("bomb")) total++;
+        console.log(squares[i]);
+      }
     }
   }
 
   createBoard();
+
+  //   Click on square actions
+  function click(square) {
+    if (square.classList.contains("bomb")) {
+      console.log("Game Over");
+    } else {
+      let total = square.getAttribute("data");
+      if (total != 0) {
+        square.classList.add("checked");
+        square.innerHTML = total;
+        return;
+      }
+    }
+  }
 });
